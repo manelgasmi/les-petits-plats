9f3374965259adf0f1dd62b6d8842533a765f72a
Manel Gasmi <manel.gasmi02@gmail.com>
2024-11-15 20:10:34 +0100

    Corriger l'erreur de l'autosave

--- scripts/factories/reciepe-factory.js
+++ scripts/factories/reciepe-factory.js
@@ -1,10 +1,12 @@
 import { Recipe } from "../classes/reciepe.js";
 
 export class RecipeFactory {
+  
   //convertir le json des recipes en array de classes Recipe
   getRecipes(recipesData) {
     return recipesData.map((recipe) => new Recipe(recipe));
   }
+
 
   getFilters(recipes) {
     const ingredients = [];
@@ -39,18 +41,16 @@ export class RecipeFactory {
   }
 
   filterRecipes(recipes, filterChoices) {
-    let filtredRecipes = [];
-    for (let i = 0; i < recipes.length; i++) {
-      const recipe = recipes[i];
-      let searchTextMatch = false,
+    return recipes.filter((recipe) => {
+        let searchTextMatch = false,
         ingredientsMatch = false,
         appliancesMatch = false,
         ustensilsMatch = false;
-
+      
       //comparer le titre, la description et les ingrédients
       //par rapport au texte inséré dans la barre de recherche principale
       if (filterChoices.general) {
-        let titleMatch = recipe.name
+        const titleMatch = recipe.name
           .toLowerCase()
           .includes(filterChoices.general);
         if (titleMatch) {
@@ -62,82 +62,49 @@ export class RecipeFactory {
           if (descriptionMatch) {
             searchTextMatch = true;
           } else {
-            let ingredientSearchtMatch = false;
-            let j = 0;
-            while (j < recipe.ingredients.length && !ingredientSearchtMatch) {
-              if (
-                recipe.ingredients[j].ingredient
-                  .toLowerCase()
-                  .includes(filterChoices.general)
-              ) {
-                ingredientSearchtMatch = true;
-              }
-              j++;
-            }
-            if (ingredientSearchtMatch) {
+            const ingredientSearchtMatch = recipe.ingredients.filter((ingredient) =>
+              ingredient.ingredient.toLowerCase().includes(filterChoices.general)
+            )
+            if (ingredientSearchtMatch.length > 0) {
               searchTextMatch = true;
             }
           }
         }
-
-        // Si aucun texte de recherche n’est fourni
       } else {
         searchTextMatch = true;
       }
 
       //comparer avec le filtre des ingrédients
       if (filterChoices.ingredients.length > 0) {
-        ingredientsMatch = true;
-        for (let e = 0; e < filterChoices.ingredients.length; e++) {
-          let ingredientFound = false;
-          for (let f = 0; f < recipe.ingredients.length; f++) {
-            if (
-              recipe.ingredients[f].ingredient.toLowerCase() ===
-              filterChoices.ingredients[e].toLowerCase()
-            ) {
-              ingredientFound = true;
-              break;
-            }
-          }
-
-          if (!ingredientFound) {
-            ingredientsMatch = false;
-            break;
-          }
-        }
+        ingredientsMatch = filterChoices.ingredients.every((ingredientName) =>
+          recipe.ingredients.some(
+            (ingredient) =>
+              ingredient.ingredient.toLowerCase() === ingredientName
+          )
+        );
+      // si auncun ingrédient n'est précisé dans le filtre
       } else {
         ingredientsMatch = true;
       }
 
       //comparer avec le filtre des appareils
       if (filterChoices.appliances.length > 0) {
-        appliancesMatch = true;
-        let l = 0;
-        while (l < filterChoices.appliances.length && appliancesMatch) {
-          if (
-            recipe.appliance.toLowerCase() !==
-            filterChoices.appliances[l].toLowerCase()
-          ) {
-            appliancesMatch = false;
-          }
-          l++;
-        }
+        appliancesMatch = filterChoices.appliances.every(
+          (applianceName) => recipe.appliance.toLowerCase() === applianceName
+        );
+     
       } else {
         appliancesMatch = true;
       }
 
       //comparer avec le filtre des ustensiles
       if (filterChoices.ustensils.length > 0) {
-        ustensilsMatch = true;
-        let m = 0;
-        while (m < filterChoices.ustensils.length && ustensilsMatch) {
-          if (
-            !recipe.ustensils.includes(filterChoices.ustensils[m].toLowerCase())
-          ) {
-            ustensilsMatch = false;
-          }
-          m++;
-        }
+        ustensilsMatch = filterChoices.ustensils.every((ustensilName) =>
+          recipe.ustensils.some(
+            (ustensil) =>
+                ustensil.toLowerCase() === ustensilName
+          )
+        );
       } else {
         ustensilsMatch = true;
       }
@@ -149,9 +116,8 @@ export class RecipeFactory {
         ustensilsMatch &&
         appliancesMatch
       ) {
-        filtredRecipes.push(recipe);
+        return recipe;
       }
-    }
-    return filtredRecipes;
+    });
   }
 }
